34012698ba69c29e560eb9066cd250a158bab4c2
HH307- <HH307-@HH315-37>
2016-11-14 10:48:06 +0900

    t

--- helloWorld/src/helloWorld/PushPrint.java
+++ helloWorld/src/helloWorld/PushPrint.java
@@ -4,6 +4,8 @@ public class PushPrint {
 	
 	public static void main(String[] args){
 		System.out.println("Add Java Class File Push");
+		System.out.println("World with 365");
+		System.out.println("World with local");
 	}
 
 }
